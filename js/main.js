6a18873097bd4858f1337dbe6771a051855f48a1
joou1232 <986573059@qq.com>
2022-07-19 16:47:32 +0100

    0719

--- js/main.js
+++ js/main.js
@@ -25,6 +25,11 @@ require([
 
   ], function (esriConfig,Map, TileLayer, MapView) {
 
+    // Add layer to map
+    var layer = new TileLayer({
+        url: "https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/clt_1980/MapServer"
+    });
+
     esriConfig.apiKey = "AAPK1ecd870c1d4e4bcfac2352462e92a51e8eqBgiOJkh37AbrcXWy2rMnSSxumpX-fgm_CxI3gGYZ0j_iKq4EnGmIoK-P-Vs1O";
 
     const map = new Map({
@@ -36,13 +41,10 @@ require([
     const view = new MapView({
       container: "viewDiv",
       center: [-2.244644,53.483959],
-      zoom: 5,
-      container: "viewDiv" 
+      zoom: 2,
+      map:map
     });
 
-    // Add layer to map
-    const layer = new TileLayer({
-        url: "https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/clt_1980/MapServer"
-      });
+
 
   });
