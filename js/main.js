eef78bc5307406f1418fa2bb0b83aa8ccb663167
joou1232 <986573059@qq.com>
2022-08-02 22:00:34 +0100

    0802_3

--- js/main.js
+++ js/main.js
@@ -10,8 +10,9 @@ require([
   "esri/widgets/Expand",
   "esri/widgets/Legend",
   "esri/widgets/Swipe",
-  "esri/widgets/TimeSlider"
-], function(Map, TileLayer,FeatureLayer,VectorTileLayer,Point, MapView,Basemap,Search,Expand,Legend,Swipe,TimeSlider) {
+  "esri/widgets/TimeSlider",
+  "esri/widgets/Home"
+], function(Map, TileLayer,FeatureLayer,VectorTileLayer,Point, MapView,Basemap,Search,Expand,Legend,Swipe,TimeSlider,Home) {
 
    const labelClass = {
           // autocasts as new LabelClass()
@@ -31,40 +32,105 @@ require([
           }
         };
   
+    // https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/wsgmax10m_future_autumn/MapServer
+
     let url_head = 'https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/';
 
-    let url_end = '_1980_spring/MapServer';
+    let url_end = '/MapServer';
 
-    var lyr_url = 'https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/clt_history_summer/MapServer';
+    var lyr_url = 'https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/clt_history_annual/MapServer';
+
+    var legend_dict = {
+      'clt': 'Cloud cover (%)',
+      'flashrate': 'Lightning (No. of flashes)',
+      'hurs': 'Relative humidity (%)',
+      'huss': 'Specific humidity (%)',
+      'pr': 'Precipitation (mm/day)',
+      'prsn': 'Snow: snowfall amount (mm)',
+      'psl': 'Sea level pressure (hPa)',
+      'rls': 'Radiation, net long wave (Wm-2)',
+      'rss': 'Radiation, net short wave (Wm-2)',
+      'sfcWind': 'Wind speed (m/s)',
+      'snw': 'Snow: lying snow amount (mm)',
+      'tas': 'Temperature, Mean (°C)',
+      'tasmax': 'Temperature, maximum (°C)',
+      'tasmin': 'Temperature, minimum (°C)',
+      'uas': 'Wind speed eastwards (m/s)',
+      'vas': 'Wind speed northwards (m/s)',
+      'wsgmax10m': 'Wind gusts (m/s)'
+    };
   
     function show_lyr() {
       var radios = document.getElementsByName('var');
-      var value;
+      var var_value;
       for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
           // do whatever you want with the checked radio
-          value = radios[i].value;
-          console.log(value)
+          var_value = radios[i].value;
+          console.log(var_value)
           
           // only one radio can be logically checked, don't check the rest
           break;
         }
       }
 
-      lyr_url = (url_head.concat(value)).concat(url_end);
-      console.log(lyr_url);
+      radios = document.getElementsByName('per');
+      var per_value;
+      for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+          // do whatever you want with the checked radio
+          per_value = radios[i].value;
+          
+          // only one radio can be logically checked, don't check the rest
+          break;
+        }
+      }
+
+      radios = document.getElementsByName('sea');
+      var sea_value;
+      for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+          // do whatever you want with the checked radio
+          sea_value = radios[i].value;
+          
+          // only one radio can be logically checked, don't check the rest
+          break;
+        }
+      }
+
+      layer_name = var_value + '_' + per_value + '_' + sea_value
+
+      lyr_url = (url_head.concat(layer_name)).concat(url_end);
   
       var vtlLayer = new TileLayer({
       url: lyr_url,
       spatialReference: 27700
       });
 
+      map.removeAll();
+
       map.layers.add(vtlLayer);
-  
+      map.layers.add(fealayer);
+      map.layers.add(vtlLayer_GB);
+
+      // view.ui.remove(legend);
+
+      view.ui.empty("bottom-right");
+
+      
+
+      view.ui.add(new Legend({
+        view: view,
+        layerInfos: [
+          {
+            layer: vtlLayer,
+            title: legend_dict[var_value]
+          }
+        ]
+      }), "bottom-right");
+
     }
 
-
- 
   
     var vtlLayer = new TileLayer({
     url: "https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/clt_history_annual/MapServer",
@@ -83,10 +149,17 @@ require([
       // labelingInfo: [labelClass],
     });
 
+    let fealayer2 = new FeatureLayer({
+      // URL to the vector tile service
+      url: "https://services1.arcgis.com/SfF67lOzKAmtSACX/arcgis/rest/services/GBR_adm2/FeatureServer"
+    });
+
     let vtlLayer_GB = new VectorTileLayer({
       // URL to the vector tile service
       url: "https://uomanchester.maps.arcgis.com/sharing/rest/content/items/796bcd7e487b416ba3420d1ffc8649d7/resources/styles/root.json"
     });
+
+
   
     let map = new Map({
       basemap: {
@@ -95,7 +168,6 @@ require([
           id: "a118075240bc4e4f8062265ecdad0e7e" // Open Grey
         }
       },
-      
       layers: [vtlLayer,fealayer,vtlLayer_GB]
     });
 
@@ -103,59 +175,72 @@ require([
       spatialReference: 27700, 
       container: "viewDiv",
       map: map,
-      center: new Point({x: 500000, y: 500000, spatialReference: 27700}),
+      center: new Point({x: 350000, y: 500000, spatialReference: 27700}),
       zoom: 4
     });
 
-  
+ 
+    // // time slider widget initialization
+    // const timeSlider = new TimeSlider({
+    //   container: "timeSlider",
+    //   view: view,
+    //   timeVisible: true, // show the time stamps on the timeslider
+    //   loop: true
+    // });
+
+    const searchWidget = new Search({
+    view: view
+    });
+
+    // Add the search widget to the top right corner of the view
+    view.ui.add(searchWidget, {
+      position: "top-right",
+      index:2,
+    });
+
+    // Add legend
+    const activeLayer = map.layers.getItemAt(0);
+
     const legend = new Legend({
-    view: view,
+      view: view,
       layerInfos: [
         {
-          layer: vtlLayer2,
-          title: "Precipitation(%)"
+          layer: activeLayer,
+          title: "Cloud cover (%)"
         }
       ]
     });
 
-    const legendExpand = new Expand({
-      expandIconClass: "esri-icon-legend",
-      expandTooltip: "Legend",
-      view: view,
-      content: legend,
-      expanded: false
+    // Add widget to the bottom right corner of the view
+    view.ui.add(legend, "bottom-right");
+
+    // Add home button
+    const homeBtn = new Home({
+      view: view
     });
 
-    view.ui.add(legendExpand, "top-left");
+    view.ui.add(homeBtn, "top-left");
 
 
+    // Add button behaviors
+    var radios = document.getElementsByName('var');
+    for (var i = 0, length = radios.length; i < length; i++) {
+      var id_name = 'var_opt'+ (i+1);
+      document.getElementById(id_name).addEventListener("click",show_lyr);
+    }
 
-            
-        // time slider widget initialization
-        const timeSlider = new TimeSlider({
-          container: "timeSlider",
-          view: view,
-          timeVisible: true, // show the time stamps on the timeslider
-          loop: true
-        });
-  
-  // document.getElementById('var').addEventListener("click",show_lyr);  
-  
-//  var checkedValue = document.querySelector('.selectopt:checked').value;
-// console.log(checkedValue);
+    for (var i = 0, length = 3; i<length; i++) {
+      var id_name = 'per_opt'+ (i+1);
+      console.log(id_name)
+      document.getElementById(id_name).addEventListener("click",show_lyr);
+    }
 
-          const searchWidget = new Search({
-          view: view
-        });
+    for (var i = 0, length = 5; i<length; i++) {
+      var id_name = 'sea_opt'+ (i+1);
+      console.log(id_name)
+      document.getElementById(id_name).addEventListener("click",show_lyr);
+    }
 
-        // Add the search widget to the top right corner of the view
-        view.ui.add(searchWidget, {
-          position: "top-right",
-          index:2,
-        });
-
-document.getElementById('opt2').addEventListener("click",show_lyr);  
-document.getElementById('opt1').addEventListener("click",show_lyr); 
 
 });
 
