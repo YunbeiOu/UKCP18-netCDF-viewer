f9e861bd90e6be62ef929541d2b12d75d6cd0583
joou1232 <986573059@qq.com>
2022-08-02 13:14:04 +0100

    0802

--- js/main.js
+++ js/main.js
@@ -1,21 +1,55 @@
 require([
   "esri/Map",
   "esri/layers/TileLayer",
+  "esri/layers/FeatureLayer",
   "esri/geometry/Point",
   "esri/views/MapView",
    "esri/Basemap",
-], function(Map, TileLayer,Point, MapView,Basemap) {
+  "esri/widgets/Search",
+  "esri/widgets/Expand",
+  "esri/widgets/Legend",
+  "esri/widgets/Swipe",
+  "esri/widgets/TimeSlider"
+], function(Map, TileLayer,FeatureLayer,Point, MapView,Basemap,Search,Expand,Legend,Swipe,TimeSlider) {
 
+   const labelClass = {
+          // autocasts as new LabelClass()
+          symbol: {
+            type: "text", // autocasts as new TextSymbol()
+            color: "white",
+            font: {
+              // autocast as new Font()
+              family: "Playfair Display",
+              size: 10,
+              weight: "bold"
+            }
+          },
+          labelPlacement: "above-center",
+          labelExpressionInfo: {
+            expression: "$feature.NAME_1"
+          }
+        };
+  
 let url_head = 'https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/';
 
 let url_end = '_1980_spring/MapServer';
 
-var lyr_url = 'https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/tas_1980_spring/MapServer';
+var lyr_url = 'https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/clt_history_summer/MapServer';
   
 function show_lyr() {
-  var select = document.getElementById('var');
-  var value = select.options[select.selectedIndex].value;
-  console.log(value); // en
+  var radios = document.getElementsByName('var');
+  var value;
+for (var i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+    // do whatever you want with the checked radio
+    value = radios[i].value;
+    console.log(value)
+    
+    // only one radio can be logically checked, don't check the rest
+    break;
+  }
+}
+
   lyr_url = (url_head.concat(value)).concat(url_end);
 console.log(lyr_url);
   
@@ -28,12 +62,24 @@ spatialReference: 27700
   
 }
 
-document.getElementById('var').addEventListener("click",show_lyr);  
+
  
   
 var vtlLayer = new TileLayer({
-url: lyr_url,
+url: "https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/clt_history_annual/MapServer",
 spatialReference: 27700
+});
+  
+var vtlLayer2 = new TileLayer({
+url: "https://tiles.arcgis.com/tiles/SfF67lOzKAmtSACX/arcgis/rest/services/pr_1980_spring/MapServer",
+spatialReference: 27700
+});
+  
+// Create featurelayer from feature service
+const fealayer = new FeatureLayer({
+  // URL to the service
+  url: "https://services1.arcgis.com/SfF67lOzKAmtSACX/arcgis/rest/services/GBR_adm1/FeatureServer",
+  labelingInfo: [labelClass],
 });
   
 let map = new Map({
@@ -54,6 +100,61 @@ let view = new MapView({
   center: new Point({x: 500000, y: 500000, spatialReference: 27700}),
   zoom: 4
 });
+
+
+  
+    // var select = document.getElementById('var');
+    // var select_text = select.options[select.selectedIndex].text;
+  
+          const legend = new Legend({
+          view: view,
+            layerInfos: [
+              {
+                layer: vtlLayer2,
+                title: "Precipitation(%)"
+              }
+            ]
+        });
+        const legendExpand = new Expand({
+          expandIconClass: "esri-icon-legend",
+          expandTooltip: "Legend",
+          view: view,
+          content: legend,
+          expanded: false
+        });
+        view.ui.add(legendExpand, "top-left");
+
+
+ 
+
+//         // add the widget to the view
+//         view.ui.add(swipe);
+            
+        // time slider widget initialization
+        const timeSlider = new TimeSlider({
+          container: "timeSlider",
+          view: view,
+          timeVisible: true, // show the time stamps on the timeslider
+          loop: true
+        });
+  
+  // document.getElementById('var').addEventListener("click",show_lyr);  
+  
+//  var checkedValue = document.querySelector('.selectopt:checked').value;
+// console.log(checkedValue);
+
+          const searchWidget = new Search({
+          view: view
+        });
+
+        // Add the search widget to the top right corner of the view
+        view.ui.add(searchWidget, {
+          position: "top-right",
+          index:2,
+        });
+
+document.getElementById('opt2').addEventListener("click",show_lyr);  
+document.getElementById('opt1').addEventListener("click",show_lyr); 
 
 });
 
